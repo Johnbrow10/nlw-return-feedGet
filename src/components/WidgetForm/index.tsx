874320cd6abb792f8a9f5dd92067a23416fd980c
johnbrow10 <iroman250@gmail.com>
2022-05-05 17:15:31 -0300

    feat(WidgetForm): separar  componentes e aplicar a comunicacao entre eles

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -1,11 +1,11 @@
-import { CloseButton } from "../CloseButton";
-
 import bugImageUrl from '../../assets/bug.svg';
 import ideaImageUrl from '../../assets/idea.svg';
 import thoughtImageUrl from '../../assets/thought.svg';
 import { useState } from "react";
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
     BUG: {
         title: 'Problema',
         image: {
@@ -30,42 +30,26 @@ const feedbackTypes = {
 
 }
 
-type FeedBackType = keyof typeof feedbackTypes
+export type FeedBackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
 
-    const [feedBackType, setFeedBackType] = useState<FeedBackType | null>(null);
+    const [feedbackType, setFeedBackType] = useState<FeedBackType | null>(null);
+
+    function handleRestartFeedback() {
+        setFeedBackType(null);
+    }
 
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-
-                <CloseButton />
-            </header>
-
-            {!feedBackType ?
+            {!feedbackType ?
                 (
-                    <div className="flex py-8 gap-2 w-full">
-                        {
-                            Object.entries(feedbackTypes).map(([key, value]) => {
-                                return (
-                                    <button
-                                        key={key}
-                                        className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex flex-col items-center gap-2 border-2 
-                                border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                                        onClick={() => setFeedBackType(key as FeedBackType)}
-                                        type="button"
-                                    >
-                                        <img src={value.image.source} alt={value.image.alt} />
-                                        <span>{value.title}</span>
-                                    </button>
-                                );
-                            })
-                        }
-                    </div>
+                    <FeedbackTypeStep onFeedbackTypeChanged={setFeedBackType} />
                 ) : (
-                    <p>Hello World</p>
+                    <FeedbackContentStep
+                        feedbackType={feedbackType}
+                        onFeedbackRestartRequested={handleRestartFeedback}
+                    />
                 )
             }
 
